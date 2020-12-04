8883417d0e2a58ba84c8bf11d4db375bd8d58220
ravlick <svtrojan16@gmail.com>
2020-12-04 22:06:36 +0200

    slider-tarif continue...

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.slider').bxSlider({
         slideWidth:371,
         maxSlides:3,
-        moveSlides:3
+        moveSlides:3,
+        pager:false
     });
 });
