db7957be85a0ca518b9d98079ae08dca53f721c2
ravlick <svtrojan16@gmail.com>
2020-12-11 18:12:03 +0200

    page pay finish

--- js/main.js
+++ js/main.js
@@ -5,4 +5,12 @@ $(document).ready(function(){
         moveSlides:3,
         pager:false
     });
+    $('.pay-slider-content').bxSlider({
+        slideWidth:220,
+        maxSlides:5,
+        moveSlides:5,
+        pager:false,
+
+    });
+
 });
